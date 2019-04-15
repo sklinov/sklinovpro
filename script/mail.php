ca27550358a3534d65c97c6a733e44361629b3ff
sklinov <sergeyklinov@gmail.com>
2019-04-15 12:55:07 +0500

    mail - template

--- script/mail.php
+++ script/mail.php
@@ -4,7 +4,54 @@ $me = "Sergei Klinov <me@sklinov.pro>";
 $subj = "You have received a message";
 $customer_subj = "Your message received";
 $body = "You have received a message from ";
-$customer_body = ", thank you for contacting me. I will respond your inquiery as soon as possible. This is an automated message but you can reply to this e-mail if you have further questions. Best wishes, Sergei Klinov";
+
+$customer_body = '
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <style>
+            body {
+                font-family: "Montserrat", sans-serif;
+                background-color: #eeeeee;
+            }
+            .msg__container {
+                margin-top: 30px;
+                margin-bottom: 30px;
+                margin-left: auto;
+                margin-right: auto;
+                padding: 20px;
+                background-color: #fafafa;
+                filter: drop-shadow(10px 10px 10px #bdbdbd);
+            }
+            .msg__header {
+                font-size: 2em;
+                font-weight: 500;
+            }
+            .msg__text {
+                font-size: 1em;
+            }
+            .msg__smalltext {
+                font-size: 0.5em;
+            }
+
+        </style>
+    </head>
+<body>
+    <div class="msg__container">
+        <div class="msg__header">'.$contact["name"].'</div>    
+        <div class="msg__text">
+            <p>
+                , thank you for contacting me. I will respond your inquiery as soon as possible. 
+            </p>
+            <p>
+                This is an automated message but you can reply to this e-mail if you have further questions. 
+            </p>
+                <br>Best wishes, <br><br> Sergei Klinov
+        </div>
+    <div>
+</body>
+</html>
+';
 
 
 $headers = 'From: Sergei Klinov <me@sklinov.pro>' . "\r\n" .
@@ -21,7 +68,7 @@ $contact["phone"] = isset($_POST['phone']) ? $_POST['phone'] : NULL;
 $contact["email"] = isset($_POST['email']) ? $_POST['email'] : NULL;
 
 $body = $body.$contact["name"]." Please respond him as soon as possible to ".$contact["email"]." or call ".$contact["phone"];
-$customer_body = $contact["name"].$customer_body;
+//$customer_body = $contact["name"].$customer_body;
 
 mail($me,$subj,$body,$headers,$additional_headers);
 mail($contact["email"],$customer_subj, $customer_body, $customer_headers, $additional_headers);
