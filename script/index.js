e59737ba8e6b5115b80998044186dd02e4aa0cd9
sklinov <sergeyklinov@gmail.com>
2019-04-15 15:46:28 +0500

    modal

--- script/index.js
+++ script/index.js
@@ -4,12 +4,14 @@ $(function() {
 
 	$('#order-call,#order-call-small,#order-call-footer,#know-more,#know-price,#know-project').click(function() {
 		messageIsSent = show_modal(messageIsSent,"Hello,");
+		console.log(messageIsSent);
 	});
+	
 	
 });
 
-function show_modal(messageIsSent,label) {
-	if(!messageIsSent)
+function show_modal(msgIsSent,label) {
+	if(!msgIsSent)
 	{
 		$('.h3.h3-modal').html(label);
 		$('.modal__form').css("height","70%");
@@ -35,8 +37,7 @@ function show_modal(messageIsSent,label) {
 					$('.modal__form').hide();
 					$('.h3.h3-modal').html(results);
 					$('#modal-call').delay(3000).fadeOut(200);
-					messageIsSent = true;
-					
+					mesgIsSent = true;	
 				},
 				error: function() {
 					console.log('ajax error');
@@ -44,14 +45,14 @@ function show_modal(messageIsSent,label) {
 			});
 				
 		});
-		return messageIsSent;
+		return msgIsSent;
 	}
-	else if(messageIsSent) {
+	else if(msgIsSent) {
 		$('.h3.h3-modal').html("Inquery already sent. Thank you!");
 		$('#modal__clear').click(function() {
 			show_modal();
 		});
 		$('#modal-call').delay(3000).fadeOut(200);
-		return messageIsSent;	
+		return msgIsSent;	
 	}
 }
