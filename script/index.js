73f1eb48383b0693cdd900041a4367997c929c1a
sklinov <sergeyklinov@gmail.com>
2019-04-15 16:43:01 +0500

    modal updated

--- script/index.js
+++ script/index.js
@@ -1,29 +1,27 @@
 $(function() {
 	//modal windows
-	var messageIsSent = false;
-
-	$('#order-call,#order-call-small,#order-call-footer,#know-more,#know-price,#know-project').click(function() {
-		messageIsSent = show_modal(messageIsSent,"Hello,");
-		console.log(messageIsSent);
+	
+	$('.call').click(function() {
+		show_modal("#modal-call");
 	});
 	
-	
+
 });
 
-function show_modal(msgIsSent,label) {
-	if(!msgIsSent)
+function show_modal(modal) {
+	
+	$(modal).show();
+	//close button
+	$('.modal__clear').click(function() {
+		$(modal).hide();//show_modal();
+	});
+	//main form contents
+	if(modal=="#modal-call")
 	{
-		$('.h3.h3-modal').html(label);
+		$('#modal-call-h3').html("Hello, ");
 		$('.modal__form').css("height","70%");
 		$('#mail, #mail-label').show();
-
-		var completed=false;
 		$('#phone').mask("+99(999)999-99-99",{placeholder:" "});
-		$('#modal-call').toggle();
-		$('#modal__clear').click(function() {
-			show_modal();
-		});
-		
 		$('#order-call-button').click(function() {
 			var formData = {};
 			formData.name=$("#name").val();
@@ -35,9 +33,8 @@ function show_modal(msgIsSent,label) {
 				data: formData,
 				success: function(results) {
 					$('.modal__form').hide();
-					$('.h3.h3-modal').html(results);
-					$('#modal-call').delay(3000).fadeOut(200);
-					mesgIsSent = true;	
+					$('#modal-call-h3').html(results);
+					$('#modal-call').delay(3000).fadeOut(200);	
 				},
 				error: function() {
 					console.log('ajax error');
@@ -45,14 +42,49 @@ function show_modal(msgIsSent,label) {
 			});
 				
 		});
-		return msgIsSent;
-	}
-	else if(msgIsSent) {
-		$('.h3.h3-modal').html("Inquery already sent. Thank you!");
-		$('#modal__clear').click(function() {
-			show_modal();
+		$('.call').click(function() {
+			show_modal("#modal-already");
 		});
-		$('#modal-call').delay(3000).fadeOut(200);
-		return msgIsSent;	
+
 	}
-}
+	else if (modal=="#modal-already")
+	{
+
+	}
+} 
+
+// function show_modal(element) {
+		
+	
+// 		$('.h3.h3-modal').html("Hello, ");
+// 		$('.modal__form').css("height","70%");
+// 		$('#mail, #mail-label').show();
+
+// 		var completed=false;
+// 		$('#phone').mask("+99(999)999-99-99",{placeholder:" "});
+// 		$(element).toggle();
+// 		$('#modal__clear').click(function() {
+// 			$(#modal-call).hide();//show_modal();
+// 		});
+		
+// 		$('#order-call-button').click(function() {
+// 			var formData = {};
+// 			formData.name=$("#name").val();
+// 			formData.phone=$("#phone").val();
+// 			formData.email=$("#mail").val();
+// 			$.ajax({
+// 				type: 'post',
+// 				url:  'script/mail.php',
+// 				data: formData,
+// 				success: function(results) {
+// 					$('.modal__form').hide();
+// 					$('.h3.h3-modal').html(results);
+// 					$('#modal-call').delay(3000).fadeOut(200);	
+// 				},
+// 				error: function() {
+// 					console.log('ajax error');
+// 				}
+// 			});
+				
+// 		});
+// 	}
